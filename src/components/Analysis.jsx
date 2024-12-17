86a75b5d4a4061dd7d60f025a1803e90b6118f46
harshnain05 <harshnain05@gmail.com>
2024-12-17 10:03:37 +0530

    changes

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -8,7 +8,7 @@ const Analysis = () => {
     <div className='api-section w-full bg-cover bg-no-repeat bg-center pb-[74px] max-lg:pb-14 max-md:pb-9 max-sm:pb-6'>
       <div className='container mx-auto'>
         <Lottie className='max-lg:hidden' animationData={bigLottie} />
-        <Lottie className='lg:hidden max-w-[400px] mx-auto' animationData={mobileLottie} />
+        <Lottie className='lg:hidmden max-w-[400px] mx-auto' animationData={mobileLottie} />
       </div>
     </div>
   )
