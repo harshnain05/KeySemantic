a9ba54df6a521b1c5b56beb6cc896404a670b1bd
harshnain05 <harshnain05@gmail.com>
2024-12-17 09:57:11 +0530

    adding lottie

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -1,8 +1,15 @@
+import Lottie from 'lottie-react'
 import React from 'react'
+import bigLottie from '../lottie/big.json'
+import mobileLottie from '../lottie/mobile.json'
+
 const Analysis = () => {
   return (
-    <div className='justify-center items-center'>
-      <img className='w-full mx-auto' src='/assets/image/png/third-sec-full.png' alt="analysis" />
+    <div className='api-section w-full bg-cover bg-no-repeat bg-center pb-[74px] max-lg:pb-14 max-md:pb-9 max-sm:pb-6'>
+      <div className='container mx-auto'>
+        <Lottie className='max-lg:hidden' animationData={bigLottie} />
+        <Lottie className='lg:hidden max-w-[400px] mx-auto' animationData={mobileLottie} />
+      </div>
     </div>
   )
 }
